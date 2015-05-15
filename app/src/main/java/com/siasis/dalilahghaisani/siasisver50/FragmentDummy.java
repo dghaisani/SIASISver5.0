6cdb52c8a41e826b1c34f1fc0ada7cf4901f5df6
Dalilah Ghaisani <dalilah.ghaisani@gmail.com>
2015-05-15 13:01:04 +0700

    siasis

---
+++ app/src/main/java/com/siasis/dalilahghaisani/siasisver50/FragmentDummy.java
@@ -0,0 +1,23 @@
+package com.siasis.dalilahghaisani.siasisver50;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentDummy extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        TextView text = new TextView(this.getActivity());
+        text.setText("Dummy Fragment");
+        text.setGravity(Gravity.CENTER);
+        return text;
+
+    }
+}
---
+++ app/src/main/java/com/siasis/dalilahghaisani/siasisver50/listener/MaterialHeadItemBackgroundOnClickListener.java
@@ -0,0 +1,9 @@
+package com.siasis.dalilahghaisani.siasisver50.listener;
+
+import com.siasis.dalilahghaisani.siasisver50.head.MaterialHeadItem;
+
+public interface MaterialHeadItemBackgroundOnClickListener {
+
+    public void onClick(MaterialHeadItem headItem);
+
+}
