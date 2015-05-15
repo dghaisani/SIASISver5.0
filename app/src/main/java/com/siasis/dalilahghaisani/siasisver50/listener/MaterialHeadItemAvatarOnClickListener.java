c5ee4d4c3f3fe91cae1d008269539288655433e2
Dalilah Ghaisani <dalilah.ghaisani@gmail.com>
2015-05-15 13:28:12 +0700

    Revert "siasis"
    This reverts commit 6cdb52c8a41e826b1c34f1fc0ada7cf4901f5df6.

--- app/src/main/java/com/siasis/dalilahghaisani/siasisver50/listener/MaterialHeadItemAvatarOnClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.siasis.dalilahghaisani.siasisver50.listener;
-
-import com.siasis.dalilahghaisani.siasisver50.head.MaterialHeadItem;
-
-public interface MaterialHeadItemAvatarOnClickListener {
-
-    public void onClick(MaterialHeadItem headItem);
-
-}
--- app/src/main/java/com/siasis/dalilahghaisani/siasisver50/listener/MaterialHeadItemBackgroundOnClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.siasis.dalilahghaisani.siasisver50.listener;
-
-import com.siasis.dalilahghaisani.siasisver50.head.MaterialHeadItem;
-
-public interface MaterialHeadItemBackgroundOnClickListener {
-
-    public void onClick(MaterialHeadItem headItem);
-
-}
